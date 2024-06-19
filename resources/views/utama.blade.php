ffca048a07bcebe482a07afedc8529cdfd7e89b2
unknown <bihet21264@elahan.com>
2024-06-19 09:52:41 +0700

    menambh

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -14,6 +14,7 @@
     
     <section>
     <h1>hello wordl</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, laborum eaque. Quo, quidem? Ipsa laboriosam voluptatem debitis provident, rem modi nostrum delectus libero facilis suscipit repellat molestias numquam, aspernatur ratione?</p>
     </section>
 </body>
 </html>
